b5b022d5ae48a1a874d8fd9270ba565cc07fd52c
Jagadeesh745 <157501965+Jagadeesh745@users.noreply.github.com>
2025-11-01 19:25:37 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -6,59 +6,43 @@ import { connectDB } from './lib/db.js';
 import userRouter from './routes/userRoutes.js';
 import messageRouter from './routes/messageRoutes.js';
 import { Server } from 'socket.io';
-
+// Create express app and http server
 const app = express();
 const server = http.createServer(app);
 
-const allowedOrigins = [
-  "https://quick-chat-frontend-lovat.vercel.app", // ✅ Your frontend domain
-];
-
-app.use(cors({
-  origin: allowedOrigins,
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"],
-  credentials: true,
-}));
-
+// Middleware setup
 export const io = new Server(server, {
-  cors: {
-    origin: allowedOrigins,
-    methods: ["GET", "POST"],
-    credentials: true,
-  },
-});
+    cors: {origin: "*"}
+})
 
-export const userSocketMap = {};
+// Store online users
+export const userSocketMap = {}
 
-io.on("connection", (socket) => {
-  const userId = socket.handshake.query.userId;
-  console.log("User Connected", userId);
+io.on("connection",(socket)=>{
+    const userId = socket.handshake.query.userId;
+    console.log("User Connected", userId);
 
-  if (userId) {
-    userSocketMap[userId] = socket.id;
-  }
-
-  io.emit("getOnlineUsers", Object.keys(userSocketMap));
-
-  socket.on("disconnect", () => {
-    console.log("User Disconnected", userId);
-    delete userSocketMap[userId];
+    if(userId){
+        userSocketMap[userId] = socket.id;
+    }
     io.emit("getOnlineUsers", Object.keys(userSocketMap));
-  });
-});
 
-app.use(express.json({ limit: "4mb" }));
+    socket.on("disconnect", ()=>{
+        console.log("User Disconnected", userId);
+        delete userSocketMap[userId];
+        io.emit("getOnlineUsers", Object.keys(userSocketMap));
+    })
+})
+app.use(express.json({limit:"4mb"}));
+app.use(cors());
 
-app.use("/api/status", (req, res) => res.send("Server is live"));
+app.use("/api/status",(req,res)=> res.send("Server is live"));
 app.use("/api/auth", userRouter);
-app.use("/api/messages", messageRouter);
-
+app.use("/api/messages", messageRouter)
 await connectDB();
-
-if (process.env.NODE_ENV !== "production") {
-  const PORT = process.env.PORT || 5000;
-  server.listen(PORT, () => console.log("Server is listening at PORT: " + PORT));
+if(process.env.NODE_ENV !== "production"){
+   const PORT = process.env.PORT || 5000;
+   server.listen(PORT, ()=> console.log("Server is listening at the PORT:"+PORT));
 }
 
-export default server;
+export default server
