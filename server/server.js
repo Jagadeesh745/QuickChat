b5c2250861a97477ce60e4b70dfb966f6bdd3f81
Jagadeesh745 <nagajagadeeshwarmahadasa@gmail.com>
2025-11-01 16:47:10 +0530

    server.js updated for vercel

--- server/server.js
+++ server/server.js
@@ -40,5 +40,9 @@ app.use("/api/status",(req,res)=> res.send("Server is live"));
 app.use("/api/auth", userRouter);
 app.use("/api/messages", messageRouter)
 await connectDB();
-const PORT = process.env.PORT || 5000;
-server.listen(PORT, ()=> console.log("Server is listening at the PORT:"+PORT));
+if(process.env.NODE_ENV !== "production"){
+   const PORT = process.env.PORT || 5000;
+   server.listen(PORT, ()=> console.log("Server is listening at the PORT:"+PORT));
+}
+
+export default server
