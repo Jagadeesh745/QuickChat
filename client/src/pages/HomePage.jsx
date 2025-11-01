25e50ac953c2e819bfe90ed07a58b0cb1d8d4111
Jagadeesh745 <157501965+Jagadeesh745@users.noreply.github.com>
2025-11-01 17:05:38 +0530

    Update HomePage.jsx

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,7 +1,7 @@
 import React, { useContext, useState } from 'react'
 import Sidebar from '../components/Sidebar'
 import ChatContainer from '../components/ChatContainer'
-import RightSidebar from '../components/RightSideBar'
+import RightSidebar from '../components/RightSidebar'
 import { AuthContext } from '../../context/AuthContext'
 import { ChatContext } from '../../context/ChatContext'
 
